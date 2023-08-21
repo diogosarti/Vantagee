f8d719ae70ce95bec79ecdd9e0d16f46a3142c88
Diogo Sarti <diogosarti13@gmail.com>
2023-08-21 20:35:32 -0300

    first commit

---
+++ src/main/java/com/osguri/techskills/TechskillsApplication.java
@@ -0,0 +1,13 @@
+package com.osguri.techskills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechskillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechskillsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osguri/techskills/TechskillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osguri.techskills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechskillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
