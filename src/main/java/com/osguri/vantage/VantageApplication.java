c7819b9b3ab4219d1f8e32d3dd374f70c1cf10d2
Diogo Sarti <diogosarti13@gmail.com>
2023-08-22 17:38:05 -0300

    Nome do projeto alterado

--- src/main/java/com/osguri/vantage/VantageApplication.java
+++ src/main/java/com/osguri/vantage/VantageApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TechskillsApplication {
+public class VantageApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TechskillsApplication.class, args);
+		SpringApplication.run(VantageApplication.class, args);
 	}
 
 }
